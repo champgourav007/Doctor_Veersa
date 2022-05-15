c39930b527cc1c23a79720ab1954978717b81e99
champgourav007 <gourav.19b131001@abes.ac.in>
2022-05-16 02:13:33 +0530

    Change the host

--- Doctor_Veersa/settings.py
+++ Doctor_Veersa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-)knuoo499m9(sbl6igyd&akv3_qjy86rcmq3y_egc*a90#)r$-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['medico-care.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
